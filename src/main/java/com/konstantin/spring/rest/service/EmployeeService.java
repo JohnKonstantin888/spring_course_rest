326691ed2565a9877026e3ef6c8b172034f8cf57
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-02-02 20:33:37 +0300

    rest app version 2

---
+++ src/main/java/com/konstantin/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.konstantin.spring.rest.service;
+
+import com.konstantin.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
